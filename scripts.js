a20015bf9067f046d2a4ac681dbcd3e618bac5ed
Molly-code-dev <mollymumbi42@gmail.com>
2020-07-22 12:38:28 +0300

    added some extra info to scripts.js

--- scripts.js
+++ scripts.js
@@ -23,3 +23,15 @@ jQuery(document).ready(function(){
         alert("This is an image.");
     });
 });
+
+$(document).ready(function(){
+    $("h1").click(function(){
+        alert("This is a header.");
+    });
+    $("p").click(function(){
+        alert("This is a paragraph.");
+    });
+    $("img").click(function(){
+        alert("This is an image.");
+    });
+});
