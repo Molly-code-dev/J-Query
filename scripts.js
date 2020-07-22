8ebf8ebaa1e4b66a0a6b9d134842c8265e74b4f3
Molly-code-dev <mollymumbi42@gmail.com>
2020-07-22 12:14:07 +0300

    added some changes to scripts.js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,7 @@ jQuery('p').click(function(){
 jQuery('img').click(function(){
     alert("This is an image")
 });
+jQuery("h1").click(function(){
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER");
+});
